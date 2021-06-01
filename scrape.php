a98560bfa3ccfb674be047cad4472a1e3e2a997c
Thiwanka Wimalasuriya <thiwankaw@gmail.com>
2021-06-01 08:54:51 +0530

    Update scrape.php

--- scrape.php
+++ scrape.php
@@ -1,18 +1,50 @@
 <?php
-    error_reporting(0);
-    $page = file_get_contents('message.html');
-    $doc = new DOMDocument();
-    $doc->loadHTML($page);
 
-    $xpath = new DOMXPath($doc);
+  error_reporting(E_ALL ^ E_WARNING);
+  $path='/Users/thiwanka/Downloads/2021_05/messages/inbox/';
 
-    $tags = $xpath->query('//div[@class="_3-96 _2pio _2lek _2lel"]');
+  //$path='/Volumes/disk/messages/2007/messages/inbox/';
 
 
-    foreach ($tags as $tag) {
-    echo trim($tag->nodeValue);
-    echo "\n";
-}    
+  $dir = new DirectoryIterator($path);
+  foreach ($dir as $fileinfo) {
+      if ($fileinfo->isDir() && !$fileinfo->isDot()) {
+          $folder =  $fileinfo->getFilename();
+          checkFolder($folder , $path);
+      }
+  }
 
-  
+
+  function checkFolder($folderName,$path) {
+      $fullPath =  $path . $folderName . '/message_1.html';
+      echo $folderName. ',';
+      //echo "\n";
+      checkMessages($fullPath);
+
+  }
+
+  function checkMessages($folderName) {
+      $page = file_get_contents($folderName);
+          $doc = new DOMDocument();
+          $doc->loadHTML($page);
+          $xpath = new DOMXPath($doc);
+          $title= $xpath->query('//title');
+
+          foreach ($title as $tag){
+          echo $tag->nodeValue.',' ;
+          }
+
+          $tags = $xpath->query('//div[@class="_3-96 _2let"]');
+          $lines = 0;
+          foreach ($tags as $tag) {
+          $lines = $lines + 1;
+
+        }
+
+        echo $lines;
+
+        echo "\n";
+  }
+
+
 ?>
