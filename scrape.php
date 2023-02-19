09d81ec5a0e7025d44caf3919a7249c9bc8b4b1c
Thiwanka Wimalasuriya <thiwanka.wimalasuriya@madmobile.com>
2023-02-19 14:05:59 +0530

    update for new fb format

--- scrape.php
+++ scrape.php
@@ -1,50 +1,49 @@
 <?php
 
-  error_reporting(E_ALL ^ E_WARNING);
-  $path='/Users/thiwanka/Downloads/2021_05/messages/inbox/';
+error_reporting(E_ALL ^ E_WARNING);
 
-  //$path='/Volumes/disk/messages/2007/messages/inbox/';
-
-
-  $dir = new DirectoryIterator($path);
-  foreach ($dir as $fileinfo) {
-      if ($fileinfo->isDir() && !$fileinfo->isDot()) {
-          $folder =  $fileinfo->getFilename();
-          checkFolder($folder , $path);
-      }
-  }
+$path='/Users/thiwanka/Downloads/2023_02/messages/inbox/';
 
 
-  function checkFolder($folderName,$path) {
-      $fullPath =  $path . $folderName . '/message_1.html';
-      echo $folderName. ',';
-      //echo "\n";
-      checkMessages($fullPath);
+$dir = new DirectoryIterator($path);
+foreach ($dir as $fileinfo) {
+    if ($fileinfo->isDir() && !$fileinfo->isDot()) {
+        $folder =  $fileinfo->getFilename();
+        checkFolder($folder , $path);
+    }
+}
 
-  }
 
-  function checkMessages($folderName) {
-      $page = file_get_contents($folderName);
-          $doc = new DOMDocument();
-          $doc->loadHTML($page);
-          $xpath = new DOMXPath($doc);
-          $title= $xpath->query('//title');
+function checkFolder($folderName,$path) {
+    $fullPath =  $path . $folderName . '/message_1.html';
+    echo $folderName. ',';
+    //echo "\n";
+    checkMessages($fullPath);
 
-          foreach ($title as $tag){
-          echo $tag->nodeValue.',' ;
-          }
+}
 
-          $tags = $xpath->query('//div[@class="_3-96 _2let"]');
-          $lines = 0;
-          foreach ($tags as $tag) {
-          $lines = $lines + 1;
+function checkMessages($folderName) {
+    $page = file_get_contents($folderName);
+    $doc = new DOMDocument();
+    $doc->loadHTML($page);
+    $xpath = new DOMXPath($doc);
+    $title= $xpath->query('//title');
 
-        }
+    foreach ($title as $tag){
+        echo $tag->nodeValue.',' ;
+    }
 
-        echo $lines;
+    $tags = $xpath->query('//div[@class="_2ph_ _a6-p"]');
+    $lines = 0;
+    foreach ($tags as $tag) {
+        $lines = $lines + 1;
 
-        echo "\n";
-  }
+    }
+
+    echo $lines;
+
+    echo "\n";
+}
 
 
 ?>
